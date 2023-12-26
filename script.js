c170a85a9861cfc412d76bdc29a5aa7655bec5c0
hmdns26 <hmd996hh@gmail.com>
2023-12-26 13:50:58 +0330

    close and open modal

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
+const closeModal =function(){
+    modal.classList.add('hidden');
+    overLay.classList.add('hidden');
+}
+const openModal =function(){
+    modal.classList.remove('hidden');
+    overLay.classList.remove('hidden');
+}
+for (let i = 0 ; i < btnOpen.length ; i++ ){
+    btnOpen[i].addEventListener('click',openModal)
+};
+btnClose.addEventListener('click',closeModal);
+overLay.addEventListener('click',closeModal)
